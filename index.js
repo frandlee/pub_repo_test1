eddbafc6ab41c1cef53dbcde9f853340896d80e2
frandlee <frandlee@gmail.com>
2024-10-03 15:00:52 +0900

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+// call slack/webhook to send a message
+import { IncomingWebhook } from "@slack/webhook";
+slackMsg = {}
+
+const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+slackMsg.send = async (msg) => {
+  await webhook.send({
+    text: msg,
+  });
+}
