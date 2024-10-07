dbafb663409c1ed49eb611c4aac3c12ec1c56eb6
frandlee <frandlee@gmail.com>
2024-10-07 10:15:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import { IncomingWebhook } from "@slack/webhook";
 slackMsg = {}
 
+password = "123456"
+
 const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
 slackMsg.send = async (msg) => {
   await webhook.send({
